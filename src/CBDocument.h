dc82ca92c102f814b1a0140a39d9291a434b69ba
Gerhard Roethlin <git@the-color-black.net>
2010-03-01 19:28:02 +0100

    Switch to Document based Project
    - Switched to a Document based project template in XCode
    - Skeleton for the document class and so on

---
+++ src/CBDocument.h
@@ -0,0 +1,15 @@
+//
+//  CBDocument.h
+//  ComicBook
+//
+//  Created by cbreak on 01.03.10.
+//  Copyright 2010 cbreak. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface CBDocument : NSDocument
+{
+}
+@end
---
+++ src/CBDocument.m
@@ -0,0 +1,51 @@
+//
+//  CBDocument.m
+//  ComicBook
+//
+//  Created by cbreak on 01.03.10.
+//  Copyright 2010 cbreak. All rights reserved.
+//
+
+#import "CBDocument.h"
+
+@implementation CBDocument
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+	{
+    }
+    return self;
+}
+
+- (NSString *)windowNibName
+{
+    return @"CBDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *) aController
+{
+    [super windowControllerDidLoadNib:aController];
+    // Add any code here that needs to be executed once the windowController has loaded the document's window.
+}
+
+- (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
+{
+    if ( outError != NULL )
+	{
+		*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+	}
+	return nil;
+}
+
+- (BOOL)readFromURL:(NSURL *)absoluteURL ofType:(NSString *)typeName error:(NSError **)outError
+{
+	//    if ( outError != NULL )
+	//{
+	//	*outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
+	//}
+    return YES;
+}
+
+@end
